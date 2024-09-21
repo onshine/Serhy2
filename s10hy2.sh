8b772592b2c38c31d5c3127afe46caabc0fca50c
onshine <95197697+onshine@users.noreply.github.com>
2024-09-21 21:04:12 +0800

    更新 s10hy2.sh

---
+++ s10hy2.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+# 介绍信息
+{
+    echo -e "\e[92m" 
+    echo "通往电脑的路不止一条，所有的信息都应该是免费的，打破电脑特权，在电脑上创造艺术和美，计算机将使生活更美好。"
+    echo "    ______                   _____               _____         "
+    echo "    ___  /_ _____  ____________  /______ ___________(_)______ _"
+    echo "    __  __ \\__  / / /__  ___/_  __/_  _ \\__  ___/__  / _  __ \`/"
+    echo "    _  / / /_  /_/ / _(__  ) / /_  /  __/_  /    _  /  / /_/ / "
+    echo "    /_/ /_/ _\\__, /  /____/  \\__/  \\___/ /_/     /_/   \\__,_/  "
+    echo "            /____/                                              "
+    echo -e "\e[0m"
+}
+
+# 获取当前用户名
+USER=$(whoami)
+USER_HOME=$(readlink -f /usr/home/$USER) # 修改为 /usr/home 目录
+WORKDIR="$USER_HOME/.nezha-agent"
+HYSTERIA_WORKDIR="$USER_HOME/.hysteria"
+
+# 创建必要的目录
+mkdir -p "$WORKDIR" "$HYSTERIA_WORKDIR"
+
+# 端口和密码
+SERVER_PORT=32710
+PASSWORD="ad9b7bee-f06b-4f8a-8b1b-b1a5830c5127"
+
+# 下载依赖文件函数
+download_dependencies() {
+  ARCH=$(uname -m)
+  DOWNLOAD_DIR="$HYSTERIA_WORKDIR"
+  mkdir -p "$DOWNLOAD_DIR"
+  FILE_INFO=()
+
+  if [[ "$ARCH" == "amd64" || "$ARCH" == "x86_64" ]]; then
+    FILE_INFO=("https://download.hysteria.network/app/latest/hysteria-linux-amd64 web")
+  else
+    echo "不支持的架构: $ARCH"
+    exit 1
+  fi
+
+  for entry in "${FILE_INFO[@]}"; do
+    URL=$(echo "$entry" | cut -d ' ' -f 1)
+    NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+    FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+    if [[ -e "$FILENAME" ]]; then
+      echo -e "\e[1;32m$FILENAME 已存在，跳过下载\e[0m"
+    else
+      curl -L -sS -o "$FILENAME" "$URL"
+      echo -e "\e[1;32m下载 $FILENAME\e[0m"
+    fi
+    chmod +x "$FILENAME"
+  done
+}
+
+# 生成证书函数
+generate_cert() {
+  openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout "$HYSTERIA_WORKDIR/server.key" -out "$HYSTERIA_WORKDIR/server.crt" -subj "/CN=bing.com" -days 36500
+}
+
+# 生成配置文件函数
+generate_config() {
+  cat << EOF > "$HYSTERIA_WORKDIR/config.yaml"
+listen: :$SERVER_PORT
+
+tls:
+  cert: $HYSTERIA_WORKDIR/server.crt
+  key: $HYSTERIA_WORKDIR/server.key
+
+auth:
+  type: password
+  password: "$PASSWORD"
+
+fastOpen: true
+
+masquerade:
+  type: proxy
+  proxy:
+    url: https://bing.com
+    rewriteHost: true
+
+transport:
+  udp:
+    hopInterval: 30s
+EOF
+}
+
+# 运行 Hysteria
+run_hysteria() {
+  if [[ -e "$HYSTERIA_WORKDIR/web" ]]; then
+    nohup "$HYSTERIA_WORKDIR/web" server "$HYSTERIA_WORKDIR/config.yaml" >/dev/null 2>&1 &
+    sleep 1
+    echo -e "\e[1;32mHysteria 正在运行\e[0m"
+  fi
+}
+
+# 获取IP地址函数
+get_ip() {
+  ipv4=$(curl -s 4.ipw.cn)
+  if [[ -n "$ipv4" ]]; then
+    HOST_IP="$ipv4"
+  else
+    ipv6=$(curl -s --max-time 1 6.ipw.cn)
+    if [[ -n "$ipv6" ]]; then
+      HOST_IP="$ipv6"
+    else
+      echo -e "\e[1;35m无法获取IPv4或IPv6地址\033[0m"
+      exit 1
+    fi
+  fi
+  echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
+}
+
+# 打印配置
+print_config() {
+  echo -e "\e[1;32mHysteria2 安装成功\033[0m"
+  echo ""
+  echo -e "\e[1;33mV2rayN或Nekobox 配置\033[0m"
+  echo -e "\e[1;32mhysteria2://$PASSWORD@$HOST_IP:$SERVER_PORT/?sni=www.bing.com&alpn=h3&insecure=1#ISP\033[0m"
+}
+
+# 主程序
+install_hysteria() {
+  download_dependencies
+  generate_cert
+  generate_config
+  run_hysteria
+  get_ip
+  print_config
+}
+
+# 开始安装 Hysteria
+install_hysteria
